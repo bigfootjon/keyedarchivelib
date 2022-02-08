03e45edae13913bf87d2b80f2d9ec1ac8b16339e
Jon Janzen <jjjonjanzen@gmail.com>
2022-02-08 04:17:50 +0000

    Oops
    fbshipit-source-id: dcd963de7

--- keyedarchivelib/api.py
+++ keyedarchivelib/api.py
@@ -14,7 +14,7 @@ if sys.version_info < (3, 8):
     )
 
     def _read_object(self, in_arg):  # type: ignore
-        self._fp.seek(in_arg)
+        self._fp.seek(self._object_offsets[in_arg])
         token = self._fp.read(1)[0]
         token_l = token & 0x0F
 
